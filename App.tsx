ec3cdb0004e99c2e0da89aed7b8b82ca1d15eab6
Nguyen Quang Truong <duwamodel@gmail.com>
2026-06-22 10:29:30 +0700

    video29

--- App.tsx
+++ App.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // string 
+  const [name, setName] = useState<string>('hoidan it');
+
+  // number 
+  const [age, setAge] = useState<number>(12)
+
+
+  // null. undifine , boolean   // không hiện ra màn hình 
+  const test = false
+
+
+  // object, array 
+  const [persion, setPersion] = useState({
+    name: 'nguyen van a',
+    age: 20
+  })
+
+  const [array, setArray] = useState([
+    {
+      name: 'nguyen van b',
+      age: 22
+    }
+  ])
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>test</Text>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{persion.name}</Text>
+        <Text style={styles.text}>{persion.age}</Text>
+        <Text style={styles.text}>{JSON.stringify(persion)}</Text>
+        <Text style={styles.text}>{JSON.stringify(array)}</Text>
       </View>
       <Text style={styles.text}>hello word
         <Text style={styles.duwatruong}> duwatruong</Text>
@@ -18,13 +48,15 @@ const styles = StyleSheet.create({
     color: 'green'
   },
   text: {
-    fontSize: 40,
+    fontSize: 30,
     color: "red"
   },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20
+  }
 });
