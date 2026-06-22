68ccd9cf7e0745f88e98da5f9a453fe25d317b3c
Nguyen Quang Truong <duwamodel@gmail.com>
2026-06-22 17:44:45 +0700

    video31

--- App.tsx
+++ App.tsx
@@ -5,9 +5,22 @@ export default function App() {
 
   const [name, setName] = useState<string>('');
 
+  const [todoList, setTTodoList] = useState([
+    { id: 1, title: "Learn React Native" },
+    { id: 2, title: "Learn React.js" },
+    { id: 3, title: "Watching Netflix" },
+    { id: 4, title: "Playing ESport" },
+    { id: 5, title: "Subscribe Hỏi Dân IT :v" },
+    { id: 6, title: "Watching Youtube" },
+    { id: 7, title: "CR 7" },
+    { id: 8, title: "Tony Kroos" },
+    { id: 9, title: "Nine" },
+    { id: 10, title: "M10" },
+  ])
 
   return (
     <View style={styles.container}>
+
       <View>
         <TextInput
           onChangeText={v => setName(v)}
@@ -20,15 +33,35 @@ export default function App() {
           style={styles.input} />
         <Text style={styles.text}>{name}</Text>
       </View>
-      <Button title='Add new'/>
-      <Text style={styles.text}>hello word
-        <Text style={styles.duwatruong}> duwatruong</Text>
-      </Text>
+
+      <Button
+        title='Add new'
+        color={'green'}
+        onPress={() => alert('tab me')}
+      />
+
+      <View style={{ marginTop: 20, borderColor: 'red', borderWidth: 1 }}>
+        {todoList.map(todo => {
+          return (
+            <Text style={styles.todo}>
+              {todo.title}
+            </Text>
+          )
+        })}
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  todo: {
+    color: 'purple',
+    backgroundColor: 'pink',
+    fontSize: 32,
+    marginBottom: 20,
+    padding: 15
+  },
   input: {
     borderColor: 'violet',
     borderWidth: 1,
