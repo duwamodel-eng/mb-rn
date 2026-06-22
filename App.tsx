4fb59163fd2c90afa3febfccb7caaa707f938c4a
Nguyen Quang Truong <duwamodel@gmail.com>
2026-06-22 17:57:41 +0700

    video32

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
@@ -40,15 +40,18 @@ export default function App() {
         onPress={() => alert('tab me')}
       />
 
-      <View style={{ marginTop: 20, borderColor: 'red', borderWidth: 1 }}>
+      <ScrollView style={{ marginTop: 20, borderColor: 'red', borderWidth: 1 }}>
         {todoList.map(todo => {
           return (
-            <Text style={styles.todo}>
+            <Text
+              key={todo.id}
+              style={styles.todo}
+            >
               {todo.title}
             </Text>
           )
         })}
-      </View>
+      </ScrollView>
 
     </View>
   );
