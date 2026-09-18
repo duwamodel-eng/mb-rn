97cdaa2a94c63e67db2e0d28cd216e4adeb5ef6f
Nguyen Quang Truong <duwamodel@gmail.com>
2026-09-18 23:44:14 +0700

    video39

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Keyboard, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
 import InputTodo from './components/todo/input.todo';
 import ListTodo from './components/todo/list.todo';
+import FlexBox from './components/todo/flexbox';
 
 export default function App() {
 
@@ -22,20 +23,21 @@ export default function App() {
   }
 
   return (
-    <TouchableWithoutFeedback
-      onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
+    <FlexBox />
+    // <TouchableWithoutFeedback
+    //   onPress={() => Keyboard.dismiss()}>
+    //   <View style={styles.container}>
 
-        <InputTodo
-          addTodo={addTodo}
-        />
+    //     <InputTodo
+    //       addTodo={addTodo}
+    //     />
 
-        <ListTodo
-          todoList={todoList}
-          deleteTodo={deletedTodo}
-        />
-      </View>
-    </TouchableWithoutFeedback>
+    //     <ListTodo
+    //       todoList={todoList}
+    //       deleteTodo={deletedTodo}
+    //     />
+    //   </View>
+    // </TouchableWithoutFeedback>
   );
 }
 
--- components/button/mine.button.tsx
+++ components/button/mine.button.tsx
@@ -21,13 +21,16 @@ const styles = StyleSheet.create({
 
 interface Iprops {
   title: string,
-  onPress: any
+  onPress: () => void
 }
 const Minebutton = (props: Iprops) => {
   const { title, onPress } = props
   return (
     <Pressable
-      style={({ pressed }) => ({ opacity: pressed === true ? 0.5 : 1 })}
+      style={({ pressed }) => ({
+        opacity: pressed === true ? 0.5 : 1,
+        alignSelf: "flex-start"
+      })}
       onPress={onPress}
     >
       <View style={styles.btnContainer}>
