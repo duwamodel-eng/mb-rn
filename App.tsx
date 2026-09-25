57cb4d4a0e81656f991f93ed1cd55f77def402ce
Nguyen Quang Truong <duwamodel@gmail.com>
2026-09-26 00:23:31 +0700

    video50

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 import { Keyboard, StyleSheet, TouchableWithoutFeedback, View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import 'react-native-gesture-handler';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 export default function App() {
 
   const Stack = createNativeStackNavigator();
   const Drawer = createDrawerNavigator();
+  const Tab = createBottomTabNavigator();
 
   function HomeScreen(props: any) {
     const navigation = props.navigation
@@ -64,6 +67,7 @@ export default function App() {
   return (
 
     <NavigationContainer>
+
       {/* <Stack.Navigator
         screenOptions={{
           headerStyle: {
@@ -89,7 +93,8 @@ export default function App() {
           })}
         />
       </Stack.Navigator> */}
-      <Drawer.Navigator initialRouteName='hoidanit'>
+
+      {/* <Drawer.Navigator initialRouteName='hoidanit'>
         <Drawer.Screen name="Article" component={DetailsScreen} />
         <Drawer.Screen
           name="hoidanit"
@@ -99,7 +104,35 @@ export default function App() {
             headerTitle: 'trang chu'
           }}
         />
-      </Drawer.Navigator>
+      </Drawer.Navigator> */}
+
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'aperture'
+                : 'aperture-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'aperture' : 'aperture-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName as any}
+              size={size} color={color}
+            />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={DetailsScreen} />
+      </Tab.Navigator>
+
+
     </NavigationContainer>
 
   );
