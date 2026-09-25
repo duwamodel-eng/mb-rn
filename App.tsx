fa05da0c35537534d736d4be89455679efe221a2
Nguyen Quang Truong <duwamodel@gmail.com>
2026-09-25 22:53:59 +0700

    video47

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Keyboard, StyleSheet, TouchableWithoutFeedback, View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 export default function App() {
 
@@ -22,14 +22,18 @@ export default function App() {
 
         <View style={{ marginVertical: 10 }}>
           <Button
-            onPress={() => navigation.navigate('hoidanit')}
+            onPress={() => navigation.navigate('hoidanit',
+              { userId: 1, name: 'eric' }
+            )}
             title="go user id=1"
           />
         </View>
 
         <View style={{ marginVertical: 10 }}>
           <Button
-            onPress={() => navigation.navigate('hoidanit')}
+            onPress={() => navigation.navigate('hoidanit',
+              { userId: 2, name: 'hoidanit' }
+            )}
             title="go user id=2"
           />
         </View>
@@ -39,11 +43,12 @@ export default function App() {
   }
 
   function DetailsScreen() {
+    const route: any = useRoute()
     const navigation: any = useNavigation()
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
-        <Text>user id = ???</Text>
+        <Text>user id ={route.params.userId}</Text>
         <Button
           onPress={() => navigation.goBack()}
           title="go back home"
